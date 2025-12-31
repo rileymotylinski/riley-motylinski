02b8707f2da6628704ae9ee200241fe2b6c33d61
Riley Motylinski <riley@motylinski.com>
2025-12-31 09:26:15 -0500

    feat: transition properties for AdminPostPreview

--- src/components/AdminPost/AdminPostPreview.tsx
+++ src/components/AdminPost/AdminPostPreview.tsx
@@ -10,9 +10,9 @@ export const AdminPostPreview: FC<Props> = ({ post }) => {
     const [toolbarOpen, setToolBarOpen] = useState(false);
     return(
         <>
-            <div className="bg-background m-5 p-2 grid grid-cols-1 grid-rows-auto rounded-xl" onMouseEnter={() => setToolBarOpen(true)} onMouseLeave={() => setToolBarOpen(false)}>
-                <p className=" p-5 bg-background h-50 w-50 overflow-clip">{post.content}</p>
-                { toolbarOpen && (<div className="rounded-xl text-center bg-background-light"> This is a toolbar</div>) }
+            <div className="group bg-background m-5 p-2 grid grid-cols-1 grid-rows-auto rounded-xl" onMouseEnter={() => setToolBarOpen(true)} onMouseLeave={() => setToolBarOpen(false)}>
+                <p className=" p-5 bg-background h-50 w-50 overflow-clip group-hover:blur-sm transition-all duration-200">{post.content}</p>
+                <div className="rounded-xl text-center bg-background-light duration-200 opacity-0 group-hover:opacity-100 group-hover:-translate-y-5 transition-all origin-top"> This is a toolbar</div>
             </div>
 
         </>
